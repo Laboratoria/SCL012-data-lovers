cca184f4fa06d3735c83aca5217a7e03c5a367de
reyesvi92@gmail.com <reyesvi92@gmail.com>
2019-12-04 12:03:40 -0300

    creando funcion de filtrar en js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
-/* Manejo de data */
+import Pokemons from './data/pokemon/pokemon.js';
 
-// esta es una función de ejemplo
+export const getPokemons = (type = undefined) => {
+  if (!type) {
+    return Pokemons;
+  }
+  return Pokemons.filter(p => p.type.map(t => t.toLowerCase()).indexOf(type.toLowerCase()) >= 0);
+};
 
-export const example = () => {
-  return 'example';
+export const fetchPokemons = () => {
+  return fetch('./data/pokemon/pokemon.json');
 };
--- src/main.js
+++ src/main.js
@@ -1,2 +1,44 @@
-import POKEMON from './data/pokemon/pokemon.js';
-console.log(POKEMON)
+ import {
+   getPokemons
+ } from './data.js';
+
+ let pokemonsFilters = [];
+
+ const buttons = document.getElementsByClassName('btn-poke');
+
+ for (let i = 0; i < buttons.length; i += 1) {
+   const btn = buttons[i];
+   btn.addEventListener('click', () => {
+     pokemonsFilters = getPokemons(btn.id);
+     renderPokemons();
+   })
+ }
+
+
+ const renderPokemons = () => {
+   document.getElementById('root').innerHTML = '';
+   for (let i = 0; i < pokemonsFilters.length; i += 1) {
+     const pokemon = pokemonsFilters[i];
+     
+     const cardPokemon = createCard(pokemon);
+     document.getElementById('root').appendChild(cardPokemon);
+   }
+ }
+
+ const createCard = (pokemon) => {
+    const cardPokemon = document.createElement('div');
+    cardPokemon.className = 'card-pokemon';
+    const imagePokemon = document.createElement('img');
+    const namePokemon = document.createElement('p');
+    namePokemon.innerHTML = pokemon.name;
+    imagePokemon.src = pokemon.img; 
+    cardPokemon.appendChild(imagePokemon);
+    cardPokemon.appendChild(namePokemon);
+    return cardPokemon;
+ }
+ 
+ const btnTypePokem = document.getElementById("tipos");
+ btnTypePokem.addEventListener('click', () => {
+
+ })
+
