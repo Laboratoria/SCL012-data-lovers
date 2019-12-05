3e396906a4a8e8d0c97932e5ed0f230dd62554fe
DaniAzul <dany.madc@gmail.com>
2019-12-05 10:12:51 -0300

    funciones de javascript ademas de html y css temporal

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 /**
  * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
-
-import { example } from './data.js';
-
-console.log(example);
-
+ * import LoL from './data/lol/lol.js'*/
+ import POTTER from './data/potter/potter.js';
+import potter from './data/potter/potter.js';
+/*console.log(POTTER);
 /*
  * console.log(POKEMON);
  * console.log(LoL);
- * console.log(POTTER)
+ console.log(POTTER)
 */
+let houseGryffindor = potter.filter(potter => {
+    return potter.house === "Gryffindor";
+})
+console.log(houseGryffindor);
+
+let houseRavenclaw = potter.filter(potter =>{
+    return potter.house === "Ravenclaw";
+})
+console.log(houseRavenclaw);
+
+let houseSlytherin = potter.filter( potter =>{
+    return potter.house === "Slytherin";    
+})
+console.log(houseSlytherin);
+
+let houseHufflepuff = potter.filter (potter =>{
+    return potter.house === "Hufflepuff";
+})
+console.log(houseHufflepuff)
+
+/*console.log(Object.values(casas));*/
