6d04c1278c7a9b07da017dcf6ca6d819819962c5
PauletteOrdenes <paulette1975xx@gmail.com>
2019-12-11 11:08:14 -0300

    filtrado en mainjs

--- src/data.js
+++ src/data.js
@@ -1,7 +1,19 @@
+import { type } from "os";
+import potter from "./data/potter/potter";
+
 /* Manejo de data */
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+export const houseSlytherin = potter.filter(elemento => elemento.house === "Slytherin");
+console.log (houseSlytherin);
+
+/* export const houseSlytherin= POTTER.filter(type = houseSlytherin => 
+  {if (!type) {
+  return potter.house === "Slytherin";
+}});
+/*
+/*
+POTTER.filter(potter => {
+  return potter.house === "Gryffindor";
+  */
